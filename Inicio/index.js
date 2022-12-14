b1b198919784b849ed9f8d48c4b718174e18cf09
SoyRandy708 <randy708ochoa@gmail.com>
2022-12-13 20:44:28 -0600

    Pantalla de Inicio version 1

---
+++ Inicio/index.js
@@ -0,0 +1,7 @@
+const IniciarJuego = document.querySelector("#iniciar")
+
+IniciarJuego.addEventListener("click", IniciarPartida)
+
+function IniciarPartida() {
+    location.href = "../Juego/juego.html"
+}
